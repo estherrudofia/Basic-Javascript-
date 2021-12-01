ff33014d32c53bdfc99bc2a7a82b5ef080348ce0
Esther Udofia <queenstarry247@gmail.com>
2021-12-01 14:58:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//object
+// let person = {
+//     name: 'Esther',
+//     age: 30
+// };
+
+// Dot Notation
+// person.name = 'Blessing';
+
+// Bracket Notation
+// person['age'] = 20
+// console.log(person);
+
+// Arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+//Functions
+// function greet(name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+
+
+// function square(number) {
+//     return number * number
+// }
+
+// console.log(square(2));
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    console.log('Yay, I love chocolate ice cream!');
+}else {
+    console.log('Awww, but chocolate is my favourite..');
+}
